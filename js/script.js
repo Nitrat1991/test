59ceffb3a3e1d6f1a8c18cd4240684bc2b66b3f0
Rinat <dangerbackoff@gmail.com>
2021-08-03 16:20:23 +0300

    While

--- js/script.js
+++ js/script.js
@@ -18,50 +18,17 @@ if (personalMovieDB.count < 10) {
     alert("Вы киноман");
 }
 
-let a, b, c, d;
+let i = 0;
 
-let moviesKeys = Object.keys(personalMovieDB.movies);
-
-while (-1 < 0) {
-    a = prompt("Один из последних фильмов?", "");
-    if (a != null) {
-        if (a.length > 0 && a.length <= 50) {
-            b = prompt("На сколько оцените фильм?", "");
-            personalMovieDB.movies[a] = b;
-            break;
-        }
+while (i < 2) {
+    let a = prompt("Один из последних фильмов?", ""),
+        b = prompt("На сколько оцените фильм?", "");
+    if (a.length > 0 && a.length <= 50 && b.length > 0 && b.length && a != null && b != null) {
+        personalMovieDB.movies[a] = b;
+        i = Object.entries(personalMovieDB.movies).length;
     }
-}
 
-while (-1 < 0) {
-    c = prompt("Один из последних фильмов?", "");
-    if (c != null) {
-        if (c.length > 0 && c.length <= 50) {
-            d = prompt("На сколько оцените фильм?", "");
-            personalMovieDB.movies[c] = d;
-            break;
-        }
-    }
 }
 
 console.log(personalMovieDB);
-console.log(moviesKeys);
-
-
-
-// const num = 50;
-
-// switch (num) {
-//     case 49:
-//         console.log('Error');
-//         break;
-//     case 100:
-//         console.log('Error');
-//         break;
-//     case 50:
-//         console.log('Good!');
-//         break;
-//     default:
-//         console.log('No no!');
-//         break;
-// }
+console.log(Object.entries(personalMovieDB.movies).length);
